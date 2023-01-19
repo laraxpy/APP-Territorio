72d60f76fe0110c8b965c0176dc3e224733e7ca2
Juan Marcelo Maldonado <laraxasd@gmail.com>
2023-01-18 21:07:12 -0300

    Agregando certificados SSL al sitio

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const router = require('./router/index')
 const app = express();
 const port = process.env.PORT || 3000;
 const {engine} = require('express-handlebars');
 const path = require('path');
+https.createServer({
+    key: fs.readFileSync('/etc/letsencrypt/live/territoriofernando.ddns.net/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/territoriofernando.ddns.net/fullchain.pem')
+}, app).listen(port, ()=> {})
+
 //Habilitamos handlebars como view
 app.engine('handlebars', engine({defaultLayout: 'layout'}));
 app.set('view engine','handlebars');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', router());
 
-app.listen(port, ()=> {
-    console.log('Servidor escuchando en puerto 3000')
-})
