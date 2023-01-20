a27bb9fbb2db0568c235f145eb62f3e7223db388
Juan Marcelo Maldonado <laraxasd@gmail.com>
2023-01-20 01:24:22 -0300

    Agregando redirect HTTTP-->HTTPS

--- index.js
+++ index.js
@@ -13,8 +13,7 @@ https.createServer({
     cert: fs.readFileSync('/etc/letsencrypt/live/territoriofernando.ddns.net/fullchain.pem')
 }, app).listen(httpsport, ()=> {})
 http.createServer(function(req, res){
-    res.writeHead(301, {Location:"https://territoriofernando.ddns.net" });//+ req.headers['host'] + req.url
-    res.end();
+    res.location('https://territoriofernando.ddns.net')
 }).listen(80);
 
 
