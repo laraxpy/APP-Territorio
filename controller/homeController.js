4c1cb01fc5747a4fd022025c3ac45de7e0f538fa
Juan Marcelo Maldonado <laraxasd@gmail.com>
2023-01-20 00:30:26 -0300

    Agregando redirect HTTTP-->HTTPS

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,4 +1,11 @@
+const { request } = require("https")
 
+exports.redirector =(req, res, next) => {
+    if(!request.secure){
+        return res.redirect("https//" + req.headers.host + req.url);
+    }
+    next();
+}
 exports.pagInicio = (req,res) => {
     res.render('inicio')
 }
--- router/index.js
+++ router/index.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const homeController = require('../controller/homeController');
 
 module.exports= ()=>{
-    router.get('/', homeController.pagInicio)
-    router.get('/miterritorio', homeController.miTerritorio)
-    router.get('/mapa', homeController.mapa)
-    router.get('/login', homeController.login)
+    router.get('/',homeController.redirector, homeController.pagInicio)
+    //router.get('/miterritorio', homeController.miTerritorio)
+    //router.get('/mapa', homeController.mapa)
+    //router.get('/login', homeController.login)
 
 
     return router;
